7fa117a3b371e06bf3b0df7720a53f5dd1d4cec0
JonasDriessen0 <114576261+JonasDriessen0@users.noreply.github.com>
2024-10-03 13:13:00 +0200

    Added crosshair and started on bullet

---
+++ DpBulletHell/Assets/Scripts/FollowMouse.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowMouse : MonoBehaviour
+{
+    private void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        mousePos.z = 0;
+        transform.position = mousePos;
+        Cursor.visible = false;
+    }
+}
